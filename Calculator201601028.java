e2d5de300dd594ea3b72881bbd9ae222648756f1
ArmstrongMakome <62277724+ArmstrongMakome@users.noreply.github.com>
2020-07-29 13:44:15 +0200

    Add files via upload

---
+++ Calculator201601028.java
@@ -0,0 +1,41 @@
+public class Calculator201601028 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int num1=3;
+        int num2=6;
+        int add,subtract, multiply;
+        double divide;
+        
+        
+        add=num1+num2;
+        subtract=num1-num2;
+        multiply=num1*num2;
+        divide=num1*num2;
+        
+        
+        
+  System.out.println(num1 + " + " + num2 + " = " + 
+  (add));
+   
+  System.out.println(num1 + " - " + num2 + " = " + 
+  (subtract));
+   
+  System.out.println(num1 + " x " + num2 + " = " + 
+  (multiply));
+   
+  System.out.println(num1 + " / " + num2 + " = " + 
+  (divide));
+ 
+        
+        
+    
+   System.out.println("Sum :"+add+"\n"+"subtract :"+subtract+"\n"+"multiply:"+multiply +"\n"+"divide :"+divide);
+
+}
+    
+}
